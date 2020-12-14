3d4b4ea1814ec9a4fd8e0558a9c1a30ab98609d4
soaresden <54243866+soaresden@users.noreply.github.com>
2020-12-14 18:50:53 +0100

    adding sounds and menu

--- PyGameBatoceRecalbox/PyGameBatoceRecalbox.py
+++ PyGameBatoceRecalbox/PyGameBatoceRecalbox.py
@@ -2,6 +2,40 @@
 import pygame
 from pygame.locals import *
 
+#CONFIG
+VERSION = '1.15'
+# CONSTANTS
+WHITE = 255, 255, 255
+BLACK = 0, 0, 0
+MATTE_BLACK = 20, 20, 20
+GREEN = 40, 175, 99
+RED = 255, 0, 0
+YELLOW = 250, 237, 39
+DARK_GREEN = 0, 128, 0
+LIGHT_BLUE = 0, 191, 255
+GREY = 204, 204, 204
+BLUE = 33, 150, 243
+BACKGROUND = 174, 222, 203
+WORLD_SHIFT_SPEED_PERCENT = 0.00135
+
+FONT_EQUINOX = "fonts/font-EquinoxRg.ttf"
+FONT_VAG = "fonts/font-vagRounded-BT-Normal.ttf"
+FONT_LIGHT = "fonts/font-OpenSans-Light.ttf"
+FONT_REG= "fonts/OpenSans-Regular.ttf"
+FONT_SEMIBOLD = "fonts/OpenSans-SemiBold.ttf"
+
+SFX_BACK = "sounds/sfx/back.wav"
+SFX_OK = "sounds/sfx/ok.wav"
+SFX_SELECTION = "sounds/sfx/selection.wav"
+
+
+CONFIG_FILE = 'config.json'
+config = {'DEBUG': False, 'menu_sound': True, 'background_music': True, 'show_fps': False, 'show_score': True,
+          'high_scores': [0, 0, 0, 0, 0, 0, 0, 0, 0]}
+music_playing = False
+delta_time = 0
+
+
 # 2 - Initialize the game
 pygame.init()
 width, height = 1280, 720
@@ -13,6 +47,11 @@ playerpos=[100,100]
 # 3 - Load images
 background = pygame.image.load("images/background/back720.png")
 pad1 = pygame.image.load("images/icons/pad.png")
+
+# 3.1 - Load audio
+pygame.mixer.music.load("sounds/music/001 Video Game Music Mix Session 1.ogg")
+pygame.mixer.music.play(-1, 0.0)
+pygame.mixer.music.set_volume(0.25)
 
 # 4 - keep looping through
 while 1:
